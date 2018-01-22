5be20eb073b52e384922ce4f31a8ed16092d5691
thato <thatom@consnet.co.za>
2018-01-22 11:38:00 +0200

    'dump'

--- code/Consnet/CustomerFilter/Block/Adminhtml/Order/View/Items/Renderer/DefaultRenderer.php
+++ code/Consnet/CustomerFilter/Block/Adminhtml/Order/View/Items/Renderer/DefaultRenderer.php
@@ -289,7 +289,7 @@ class DefaultRenderer extends \Magento\Sales\Block\Adminhtml\Items\Renderer\Defa
                 break; */
             case 'price':
                 $html = round($item->getPrice(),2);
-                var_dump($html);
+                var_dump($html);die();
                 break;
             case 'qty':
                 $html = round($item->getQtyOrdered());
