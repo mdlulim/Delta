372893179c96c16d06c77c326d0d6d65cc565c8b
mdu <mduduzi@consnet.co.za>
2018-01-23 17:40:04 +0200

    company plugin

--- code/Consnet/Company/Plugin/Magento/Company/Model/ResourceModel/Company/Collection.php
+++ code/Consnet/Company/Plugin/Magento/Company/Model/ResourceModel/Company/Collection.php
@@ -12,9 +12,8 @@ class Collection
         $userId=$authSession->getUser()->getUserId();
         $roleId= $authSession->getUser()->getRole()->getRoleId();
         $roleName= $authSession->getUser()->getRole()->getRoleName();
-        var_dump(get_class_methods($authSession));
         if($roleName == 'sales_rep'){
-            return $result->getSelect()->where('sales_representative_id ='.$userId.'');
+            return $result->getSelect()->where('sales_representative_id ='.$userId.' ');
         }else{
             return $result;
         }
