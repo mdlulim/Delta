f048aa769dd9c447468727a2aedc80b96468e137
mdu <mduduzi@consnet.co.za>
2018-01-23 17:19:55 +0200

    company plugin

--- code/Consnet/Company/Plugin/Magento/Company/Model/ResourceModel/Company/Collection.php
+++ code/Consnet/Company/Plugin/Magento/Company/Model/ResourceModel/Company/Collection.php
@@ -7,6 +7,17 @@ class Collection
 {
 
     public function afterJoinAdvancedCustomerEntityTable($result) {
-       return $result->getSelect()->where('sales_representative_id = 5');
+        $om = \Magento\Framework\App\ObjectManager::getInstance();
+        $authSession = $om->get('\Magento\Backend\Model\Auth\Session');
+        $userId=$authSession->getUser()->getUserId();
+        $roleId= $authSession->getUser()->getRole()->getRoleId();
+        $roleName= $authSession->getUser()->getRole()->getRoleName();
+        var_dump(get_class_methods($authSession));
+        if($roleName == 'sales_rep'){
+            return $result->getSelect()->where('sales_representative_id ='.$userId.'');
+        }else{
+            return $result;
+        }
+        
     }
 }
