0675a49dc2dd81e3e72a12bbc1efe38cd1cd3e73
mdu <mduduzi@consnet.co.za>
2018-01-24 16:54:09 +0200

    customer filter

--- code/Consnet/CustomerFilter/Ui/Component/DataProvider.php
+++ code/Consnet/CustomerFilter/Ui/Component/DataProvider.php
@@ -58,8 +58,7 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
     /**
      * {@inheritdoc}
      */
-    public function getData()
-    {
+    public function getData() {
         $data = parent::getData();
         foreach ($this->attributeRepository->getList() as $attributeCode => $attributeData) {
             foreach ($data['items'] as &$item) {
@@ -68,7 +67,40 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
                 }
             }
         }
-        $Arra = array(1, 2, 3);
+
+        $om = \Magento\Framework\App\ObjectManager::getInstance();
+        $authSession = $om->get('\Magento\Backend\Model\Auth\Session');
+        $user = $authSession->getUser();
+        $userId = $user->getUserId();
+        $roleId = $authSession->getUser()->getRole()->getRoleId();
+       
+        $resource = $om->get('Magento\Framework\App\ResourceConnection');
+        $connection = $resource->getConnection();
+        $tableName = $resource->getTableName('company'); 
+        $tableName2 = $resource->getTableName('company_advanced_customer_entity'); 
+        
+        if(($user->getRole()->getRoleName() == 'sales_rep')) {
+            $tableName = $resource->getTableName('company'); 
+            $tableName2 = $resource->getTableName('company_advanced_customer_entity'); 
+
+            $authSession = $om->get('\Magento\Backend\Model\Auth\Session');
+            $userId=$authSession->getUser()->getUserId(); 
+            
+            $sql = "Select entity_id FROM " . $tableName." WHERE  sales_representative_id =".$userId;
+            $result = $connection->fetchAll($sql); 
+        
+            $arrayIds = [];
+            if($result != null){
+                $sql2 = "Select customer_id FROM " . $tableName2.' WHERE  company_id IN (' . implode(',', $arrayIds) . ')';
+                $result2 = $connection->fetchAll($sql2); 
+                $customerIds = [];
+                $ids=0;
+                foreach($result2 as $arr){
+                $customerIds[$ids]=$arr['customer_id'];
+                $ids++;
+                }
+            }
+            $Arra = $arrayIds;
         $tmp['items'] = array ();
 
         foreach($data['items'] as $line){
@@ -78,6 +110,10 @@ class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvi
         }
         $data = $tmp;
         return $data;
+        }else{
+          return $data;
+        } 
+  
     }
 }
 
