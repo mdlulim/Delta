3925fd72e38e3409991efaaa44fbfe1dacacc657
thato <thatom@consnet.co.za>
2018-01-24 09:41:46 +0200

    Order Item Check

--- code/Consnet/Erporder/Plugin/ErpOrder.php
+++ code/Consnet/Erporder/Plugin/ErpOrder.php
@@ -204,7 +204,7 @@ class ErpOrder
     
                     $totalTax = 0.0;
                     if($zresults->ZRESULT !== 'FAILED'){
-                        if(property_exists($zresults->ZTT_ORDER_TOTALS, 'item')){
+                        if(is_array($zresults->ZTT_ORDER_TOTALS->item)){
                             if(is_array($zresults->ZTT_ORDER_TOTALS->item)){
                                 foreach($zresults->ZTT_ORDER_TOTALS->item as $orderitm){
                                     $totalTax = $totalTax + $orderitm->MWSBP ;
