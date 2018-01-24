09a9dc27bc1ebabf5bc3ae8df5321d4663c21e3a
mdu <mduduzi@consnet.co.za>
2018-01-24 14:56:12 +0200

    customer filter

--- code/Consnet/CustomerFilter/Plugin/Magento/Customer/Ui/Component/DataProvider.php
+++ code/Consnet/CustomerFilter/Plugin/Magento/Customer/Ui/Component/DataProvider.php
@@ -7,17 +7,18 @@ class DataProvider
 {
 
     public function afterGetData($result) {
-        var_dump($result);die();
-        $data['items'] = $result;
-        $Arra = array(1, 2, 3);
-        $tmp['items'] = array ();
+        // var_dump($result);die();
+        // $data['items'] = $result;
+        // $Arra = array(1, 2, 3);
+        // $tmp['items'] = array ();
         
-        foreach($data['items'] as $line){
-            if(in_array($line['entity_id'],$Arra)){
-              array_push($tmp['items'],$line);
-            }
-        }
-        $data = $tmp;
-        return $data;
+        // foreach($data['items'] as $line){
+        //     if(in_array($line['entity_id'],$Arra)){
+        //       array_push($tmp['items'],$line);
+        //     }
+        // }
+        // $data = $tmp;
+        // return $data;
+        return $result->addAttributeToFiler('entity_id',array('eq'=>3));
     }
 }
