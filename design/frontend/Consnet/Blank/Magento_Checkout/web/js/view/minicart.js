95fe84dbac249cef91ebb8905c97e89ba8c644f6
thato <thatom@consnet.co.za>
2018-02-06 10:25:52 +0200

    top menu cart js

--- design/frontend/Consnet/Blank/Magento_Checkout/web/js/view/minicart.js
+++ design/frontend/Consnet/Blank/Magento_Checkout/web/js/view/minicart.js
@@ -187,8 +187,15 @@ define([
         getCartLineItemsCount: function () {
             var items = this.getCartParam('items') || [];
             if(items.length == 0){
-                alert(hide);
+                console.log('hide');
             }
+            console.log(items);
+            var total = 0;
+            for (var i = 0; i < items.length; i++) {
+                total += items[i].qty;
+            }
+            console.log(total);
+            document.getElementById('itemcounter').textContent = total;
             return parseInt(items.length, 10);
         }
     });
