50b824429d6faff5296f4d59f9b7547b2d7203e0
thato <thatom@consnet.co.za>
2018-02-20 10:08:59 +0200

    admin order message

--- code/Consnet/OrderExtended/Plugin/Message.php
+++ code/Consnet/OrderExtended/Plugin/Message.php
@@ -11,8 +11,8 @@ namespace Consnet\OrderExtended\Plugin;
 class Message 
 {
     public function beforeAddError($error){
-        if($error == 'Please specify a payment method.'){
-            return 'Please Select Calculate Total Option.';
+        if($error == 'Please specify a shipping method.'){
+            return ['Please Select Calculate Grand Total.'];
         }
         return null;
     }
